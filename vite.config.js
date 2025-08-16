87e563ceebaf82883193319496169fb009688ecb
Victor Hugo Giraldez Taco <victor1999.work@gmail.com>
2025-08-16 13:50:38 -0500

    acttualizacion css

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css', 
-                'public/css/princi.css',  // ← line de css
+                'resources/css/app.css',
                 'resources/js/app.js'
             ],
             refresh: true,
