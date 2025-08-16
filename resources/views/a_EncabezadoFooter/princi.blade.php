8c10d67a5f46cf2cab7beeb1a9f59fd7e43b9e01
Victor Hugo Giraldez Taco <victor1999.work@gmail.com>
2025-08-16 14:29:11 -0500

    Actualizacion de css

--- resources/views/a_EncabezadoFooter/princi.blade.php
+++ resources/views/a_EncabezadoFooter/princi.blade.php
@@ -60,9 +60,8 @@
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
             <!--<link rel="stylesheet" id="frase-style-css" href="./style.css" media="all">
-            <link rel="stylesheet" href="{{ asset('public/css/foot.css') }}">
-            <link rel="stylesheet" href="{{ asset('resources/css/foot.css') }}"> -->
-            <link rel="stylesheet" href="{{ asset('css/foot.css') }}">
+            <link rel="stylesheet" href="{{ asset('public/css/foot.css') }}">-->
+            <link rel="stylesheet" href="{{ asset('resources/css/foot.css') }}"> 
 <!--
 <style>
         :root {
