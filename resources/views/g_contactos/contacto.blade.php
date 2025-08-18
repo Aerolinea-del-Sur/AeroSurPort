7b7228f020b5f268b188405e440ec536c836453c
Victor Hugo Giraldez Taco <victor1999.work@gmail.com>
2025-08-17 22:40:39 -0500

    Debug de css de contacto

--- resources/views/g_contactos/contacto.blade.php
+++ resources/views/g_contactos/contacto.blade.php
@@ -1,7 +1,7 @@
 @extends('a_EncabezadoFooter.princi')
 @section('content')
-        <link rel="stylesheet" href="{{ asset('public/css/contacto.css') }}">
-    <!-- Elementos flotantes decorativos -->
+        
+    <!-- Elementos flotantes decorativos <link rel="stylesheet" href="{{ asset('public/css/contacto.css') }}">-->
     <div class="floating-elements">
         <div class="floating-circle floating-circle-1"></div>
         <div class="floating-circle floating-circle-2"></div>
