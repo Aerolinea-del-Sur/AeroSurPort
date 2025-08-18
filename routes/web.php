f981f49074bc3caf684071ab34b1c5b697d6d501
Victor Hugo Giraldez Taco <victor1999.work@gmail.com>
2025-08-17 22:37:41 -0500

    Actualizacion de web.php sobre rutas basicas

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 // Página principal
-Route::get('/public_html/public_html/AeroSurPort', function () {
+Route::get('/public_html/public_html/AeroSurPort/a_EncabezadoFooter/princi', function () {
     return view('a_EncabezadoFooter.princi');
 })->name('home');
 // Página principal
-Route::get('/public_html/public_html/AeroSurPort', function () {
+Route::get('/public_html/public_html/AeroSurPort/a_EncabezadoFooter/inicio', function () {
     return view('a_EncabezadoFooter.inicio');
 })->name('inicio');
 
